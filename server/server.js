40e9ca0805da0abcd165a5e6631479eabddd6987
ManinderManan <manindersmanan@gmail.com>
2023-07-21 22:29:24 -0700

    resolved conflicts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 // npm install @apollo/server express graphql cors body-parser
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
@@ -9,15 +8,20 @@ import cors from "cors";
 import typeDefs from "./schema/typeDefs.js";
 import resolvers from "./schema/resolvers.js";
 import connection from "./config/connection.js";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const server = new ApolloServer({
   typeDefs,
@@ -26,25 +30,41 @@ const server = new ApolloServer({
 });
 await server.start();
 
-const staticPath = path.join(__dirname, "../client/build/");
-console.log({ staticPath });
-
-app.use(express.static(staticPath));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 
 app.use(
   "/graphql",
-  cors({ origin: "*" }),
-  express.json(),
   expressMiddleware(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
   })
 );
 
-app.get("*", (req, res) => {
-  res.sendFile(staticPath + "/index.html");
+app.post("/api/fetchplant", async (req, res) => {
+  const key = process.env.APIKEY;
+  const url = `https://house-plants2.p.rapidapi.com/search?query=${req.body.searchTerm}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": key,
+      "X-RapidAPI-Host": "house-plants2.p.rapidapi.com",
+    },
+  };
+  const response = await fetch(url, options);
+  if (response.ok) {
+    const data = await response.json();
+
+    res.send({ data });
+  } else {
+    console.error("failed");
+  }
 });
 await connection;
-
-const PORT = process.env.PORT ?? 4000;
-await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
-console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+await new Promise((resolve) =>
+  httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
