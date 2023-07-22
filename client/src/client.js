9bff8d9dd5818ea2d079ebb97b320a62fcb0d6e7
ManinderManan <manindersmanan@gmail.com>
2023-07-21 22:33:08 -0700

    updated client.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,27 +1,9 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache} from "@apollo/client";
 
-import { setContext } from "@apollo/client/link/context";
 
-const uri = process.env.REACT_APP_GRAPHQL_ENDPOINT ?? "http://localhost:4000/graphql";
-
-const httpLink = createHttpLink({
-  uri,
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? token : "",
-    },
-  };
-});
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+ 
   cache: new InMemoryCache(),
 });
 
