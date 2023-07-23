98a60522b14040e02dccf92944194b0369296ca9
letmego1st <imcoding4real@gmail.com>
2023-07-23 14:01:09 -0700

    use shells in home to show login component or mygarden component based on loggedIn state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,43 +3,53 @@ import "./Home.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import homeImage from "./../images/PlantasticLogo.png";
+import useAuthService from "../utils/authHook";
+import { LoggedIn, NotLoggedIn } from "../components/shells";
+import MyGarden from "./MyGarden";
 
 export default function Home() {
-  return (
-    <Box
-      className="Home"
-      sx={{ display: "flex", placeContent: "center", direction: "row" }}
-    >
-      <Box className="imageLogo" sx={{ height: "500px" }}>
-        <img
-          src={homeImage}
-          alt="Plantastic Logo"
-          style={{ objectFit: "cover", width: "100%", height: "100%" }}
-        />
-        <Stack direction="row" sx={{ justifyContent: "center", gap: 2}}>
-          <Button
-            className="login"
-            variant="contained"
-            component={NavLink}
-            to="/Login"
-            color="success"
-          >
-            Login
-          </Button>
-          <Button
-            className="signUp"
-            variant="contained"
-            component={NavLink}
-            to="/signup"
-            color="success"
-          >
-            Sign Up
-          </Button>
-        </Stack>
-      </Box>
+  const auth = useAuthService();
 
-      {/* two columns */}
-      {/* <Box className="callToAction">
+  return (
+    <>
+      <LoggedIn>
+        <MyGarden />
+      </LoggedIn>
+      <NotLoggedIn>
+        <Box
+          className="Home"
+          sx={{ display: "flex", placeContent: "center", direction: "row" }}
+        >
+          <Box className="imageLogo" sx={{ height: "500px" }}>
+            <img
+              src={homeImage}
+              alt="Plantastic Logo"
+              style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+            <Stack direction="row" sx={{ justifyContent: "center", gap: 2 }}>
+              <Button
+                className="login"
+                variant="contained"
+                component={NavLink}
+                to="/Login"
+                color="success"
+              >
+                Login
+              </Button>
+              <Button
+                className="signUp"
+                variant="contained"
+                component={NavLink}
+                to="/signup"
+                color="success"
+              >
+                Sign Up
+              </Button>
+            </Stack>
+          </Box>
+
+          {/* two columns */}
+          {/* <Box className="callToAction">
         <h2>some heading</h2>
         <p>some caption goes here</p>
         <p>
@@ -50,6 +60,8 @@ export default function Home() {
         </p>
        
       </Box> */}
-    </Box>
+        </Box>
+      </NotLoggedIn>
+    </>
   );
 } //We updated a lot of the UI
