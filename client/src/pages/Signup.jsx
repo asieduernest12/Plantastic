7055f63b57015d44ab572a2e136daf48c32670a8
pvmend <mendoza.v.paul@gmail.com>
2023-07-24 07:37:13 -0700

    removed notification on mutation for plant

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -63,6 +63,10 @@ export default function SignUp() {
       isValidEmail(signUpForm.email);
     /* sets state value to disable button */
     setDisabled(!valid);
+    console.log( valid );
+    console.log(signUpForm);
+    console.log(isValidPassword(signUpForm.password))
+    console.log(isValidEmail(signUpForm.email))
   }, [signUpForm]);
 
   function handleChange(event) {
