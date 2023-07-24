5d451698eb3d968c62710474ecb3c1f57149590c
linuxdev <asieduernest@yahoo.co.uk>
2023-07-24 04:32:37 -0400

    update appcontext value

--- client/src/utils/appContext.jsx
+++ client/src/utils/appContext.jsx
@@ -1,45 +1,46 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer } from 'react';
 
 // make context base
 /** @type {{tooken:undefined|string, user: {email, _id,}}} */
 const baseContext = {
-  token: undefined,
-  user: undefined,
+	token: undefined,
+	user: undefined,
 };
 
 const dispatch = (action) => {};
 
 const initialContext = {
-  ...baseContext,
-  dispatch,
+	...baseContext,
+	dispatch,
 };
 // make context
 const appContext = createContext(initialContext);
 
 // make reducer
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_USER":
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case "SET_TOKEN":
-      return {
-        ...state,
-        token: action.payload,
-      };
+	switch (action.type) {
+		case 'SET_USER':
+			return {
+				...state,
+				user: action.payload,
+			};
+		case 'SET_TOKEN':
+			return {
+				...state,
+				token: action.payload,
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
 // make context provider
 export const AppContextProvider = ({ children }) => {
-  const res = useReducer(reducer, baseContext);
+	const [state, dispatch] = useReducer(reducer, baseContext);
 
-  return <appContext.Provider value={undefined}>{children}</appContext.Provider>;
+	const value = { ...state, dispatch };
+	return <appContext.Provider value={value}>{children}</appContext.Provider>;
 };
 
 export default appContext;
