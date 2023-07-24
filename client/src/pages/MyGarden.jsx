fc70524d87cc1fb2bd7fcf0449f79ad4db69b839
linuxdev <asieduernest@yahoo.co.uk>
2023-07-24 04:33:59 -0400

    fix refesh on create/delete

--- client/src/pages/MyGarden.jsx
+++ client/src/pages/MyGarden.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import useAuthService from "../utils/authHook";
-import { Box, Grid, IconButton, Popover, Stack, Typography } from "@mui/material";
-import { ResponsiveImageContainer } from "../components/ResponsiveImageContainer";
-import PlantDetails from "./PlantDetails";
-import { useMutation, useQuery } from "@apollo/client";
-import { QUERY_PLANTS } from "../utils/queries";
-import { NavLink } from "react-router-dom";
-import CenteredCircularProgress from "../components/CenteredCircularProgress";
-import { DeleteOutlineRounded, DeleteRounded } from "@mui/icons-material";
-import { DELETE_PLANT } from "../utils/mutations";
+import React, { useState } from 'react';
+import useAuthService from '../utils/authHook';
+import { Box, Grid, IconButton, Popover, Stack, Typography } from '@mui/material';
+import { ResponsiveImageContainer } from '../components/ResponsiveImageContainer';
+import PlantDetails from './PlantDetails';
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_PLANTS } from '../utils/queries';
+import { NavLink } from 'react-router-dom';
+import CenteredCircularProgress from '../components/CenteredCircularProgress';
+import { DeleteOutlineRounded, DeleteRounded } from '@mui/icons-material';
+import { DELETE_PLANT } from '../utils/mutations';
 
 const fakePlants = Array(5)
-  .fill(null)
-  .map(() => ({
-    name: "plant",
-    _id: crypto.getRandomValues(new Uint32Array(1))[0],
-    image: "https://via.placeholder.com/150/flower",
-  }));
+	.fill(null)
+	.map(() => ({
+		name: 'plant',
+		_id: crypto.getRandomValues(new Uint32Array(1))[0],
+		image: 'https://via.placeholder.com/150/flower',
+	}));
 
 /**
  *
@@ -24,87 +24,72 @@ const fakePlants = Array(5)
  * @returns
  */
 function MyGarden(props) {
-  const auth = useAuthService();
-  const { data, error, loading } = useQuery(QUERY_PLANTS);
-  const [deleteMutation] = useMutation(DELETE_PLANT, {
-    refetchQueries: [{ query: QUERY_PLANTS }],
-    onError: (e) => {
-      /* TO DO: add error handling */
-      console.error(e.message);
-      alert("Error deleting plant: " + e.message);
-    },
-  });
+	const auth = useAuthService();
+	const { data, error, loading } = useQuery(QUERY_PLANTS);
+	const [deleteMutation] = useMutation(DELETE_PLANT, {
+		refetchQueries: [{ query: QUERY_PLANTS }],
+		onError: (e) => {
+			/* TO DO: add error handling */
+			console.error(e.message);
+			alert('Error deleting plant: ' + e.message);
+		},
+	});
 
-  if (loading) {
-    return <CenteredCircularProgress />;
-  }
+	if (loading) {
+		return <CenteredCircularProgress />;
+	}
 
-  const [firstPlant, ...restOfPlants] = data?.plants;
+	const restOfPlants = data?.plants;
 
-  const deletePlant = (plant, e) => {
-    e.preventDefault();
-    console.log("delete plant", plant);
-    deleteMutation({ variables: { id: plant._id } }).then();
-  };
+	const deletePlant = (plant, e) => {
+		e.preventDefault();
+		console.log('delete plant', plant);
+		deleteMutation({ variables: { id: plant._id } }).then();
+	};
 
-  return (
-    <Stack className="MyGarden" sx={{ direction: "row", placeItems: "center" }}>
-      <Stack sx={{ p: 5, width: "clamp(300px,60%,700px)" }}>
-        <Box className="myGardenHeader">
-          <p>Hello</p>
-          <Typography variant="h1">{auth.user.username}</Typography>
-        </Box>
+	return (
+		<Stack className='MyGarden' sx={{ direction: 'row', placeItems: 'center' }}>
+			<Stack sx={{ p: 5, width: 'clamp(300px,60%,700px)' }}>
+				<Box className='myGardenHeader'>
+					<p>Hello</p>
+					<Typography variant='h1'>{auth.user.username}</Typography>
+				</Box>
 
-        {/* plants list in a 2/2 grid */}
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            gridTemplateRows: "200px",
-            gap: 2,
-          }}
-        >
-          {/* first plant takes 2 columns and 2 rows*/}
-          <Box sx={{ gridColumn: "1 / 3", gridRow: "1 / 3", maxHeight: "400px", position: "relative" }}>
-            <NavLink to={`/plants/${firstPlant._id}`}>
-              <ResponsiveImageContainer image={firstPlant.img} />
-              <FloatViewDetails />
-              <IconButton
-                sx={{ position: "absolute", right: 2, top: 2, color: "white", backgroundColor: "grey" }}
-                onClick={(e) => deletePlant(firstPlant, e)}
-              >
-                <DeleteRounded />
-              </IconButton>
-            </NavLink>
-          </Box>
+				{/* plants list in a 2/2 grid */}
+				<Box
+					sx={{
+						display: 'grid',
+						gridTemplateColumns: '1fr 1fr',
+						gridTemplateRows: '200px',
+						gap: 2,
+					}}
+				>
+					{/* first plant takes 2 columns and 2 rows*/}
 
-          {/* rest of the plants take 1 column and 1 row */}
-          {restOfPlants.map((plant) => (
-            <Box sx={{ position: "relative" }}>
-              <NavLink to={`/plants/${plant._id}`} key={plant._id}>
-                <ResponsiveImageContainer key={plant._id} image={plant.img} />
-                <FloatViewDetails />
-                <IconButton
-                  sx={{ position: "absolute", right: 2, top: 2, color: "white", backgroundColor: "grey" }}
-                  onClick={(e) => deletePlant(plant, e)}
-                >
-                  <DeleteRounded />
-                </IconButton>
-              </NavLink>
-            </Box>
-          ))}
-        </Box>
-      </Stack>
-    </Stack>
-  );
+					{/* rest of the plants take 1 column and 1 row */}
+					{restOfPlants.map((plant, index) => (
+						<Box sx={{ position: 'relative', ...(!index ? { gridColumn: '1 / 3', gridRow: '1 / 3', maxHeight: '400px' } : {}) }}>
+							<NavLink to={`/plants/${plant._id}`} key={plant._id}>
+								<ResponsiveImageContainer key={plant._id} image={plant.img} />
+								<FloatViewDetails />
+								<IconButton sx={{ position: 'absolute', right: 2, top: 2, color: 'white', backgroundColor: 'grey' }} onClick={(e) => deletePlant(plant, e)}>
+									<DeleteRounded />
+								</IconButton>
+							</NavLink>
+						</Box>
+					))}
+				</Box>
+			</Stack>
+		</Stack>
+	);
 }
 
 export default MyGarden;
 
 function FloatViewDetails() {
-  return (
-    <Box sx={{ display: "grid", placeContent: "center", position: "absolute", inset: "0", height: "100%", width: "100%", color: "white" }}>
-      <Typography variant="h2">View Details</Typography>
-    </Box>
-  );
+	return (
+		<Box sx={{ display: 'grid', placeContent: 'center', position: 'absolute', inset: '0', height: '100%', width: '100%', color: 'white' }}>
+			<Typography variant='h2'>View Details</Typography>
+		</Box>
+	);
 }
--- client/src/pages/PlantDetails.jsx
+++ client/src/pages/PlantDetails.jsx
@@ -1,12 +1,12 @@
-import { useLazyQuery, useMutation } from "@apollo/client";
-import { Delete } from "@mui/icons-material";
-import { Box, Button, CircularProgress, FormControl, IconButton, Paper, Stack, TextareaAutosize, Typography } from "@mui/material";
-import React, { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { ResponsiveImageContainer } from "../components/ResponsiveImageContainer";
-import { QUERY_PLANT } from "../utils/queries";
-import CenteredCircularProgress from "../components/CenteredCircularProgress";
-import { ADD_PLANT_NOTE, DELETE_PLANT_NOTE } from "../utils/mutations";
+import { useLazyQuery, useMutation } from '@apollo/client';
+import { Delete } from '@mui/icons-material';
+import { Box, Button, CircularProgress, FormControl, IconButton, Paper, Stack, TextareaAutosize, Typography } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ResponsiveImageContainer } from '../components/ResponsiveImageContainer';
+import { QUERY_PLANT } from '../utils/queries';
+import CenteredCircularProgress from '../components/CenteredCircularProgress';
+import { ADD_PLANT_NOTE, DELETE_PLANT_NOTE } from '../utils/mutations';
 
 /**
  *
@@ -14,130 +14,129 @@ import { ADD_PLANT_NOTE, DELETE_PLANT_NOTE } from "../utils/mutations";
  * @returns
  */
 function PlantDetails() {
-  const params = useParams();
-  const [query, { error, loading }] = useLazyQuery(QUERY_PLANT);
-  const [addNoteMutation] = useMutation(ADD_PLANT_NOTE, {
-    onError: (e) => {
-      /* TO DO: add error handling */
-      console.error(e.message);
-      alert("Error adding note: " + e.message);
-    },
-  });
+	const params = useParams();
+	const [query, { error, loading }] = useLazyQuery(QUERY_PLANT, {
+		onCompleted: (res) => {
+			console.log('complete', res);
+			setPlant(res.plant);
+		},
+	});
+	const [addNoteMutation] = useMutation(ADD_PLANT_NOTE, {
+		refetchQueries: [{ query: QUERY_PLANT, variables: { id: params.id } }],
+		onError: (e) => {
+			/* TO DO: add error handling */
+			console.error(e.message);
+			alert('Error adding note: ' + e.message);
+		},
+	});
 
-  const [deletePlantNoteMutation] = useMutation(DELETE_PLANT_NOTE, {
-    onError: (e) => {
-      /* TO DO: add error handling */
-      console.error(e.message);
-      alert("Error deleting note: " + e.message);
-    },
-  });
+	const [deletePlantNoteMutation] = useMutation(DELETE_PLANT_NOTE, {
+		refetchQueries: [{ query: QUERY_PLANT, variables: { id: params.id } }],
+		onError: (e) => {
+			/* TO DO: add error handling */
+			console.error(e.message);
+			alert('Error deleting note: ' + e.message);
+		},
+	});
 
-  const [plant, setPlant] = useState(undefined);
+	const [plant, setPlant] = useState(undefined);
 
-  const [notes, setNotes] = useState(undefined);
+	const [notes, setNotes] = useState(undefined);
 
-  const handleAddNote = (e) => {
-    e.preventDefault();
-    setNotes([...(notes ?? []), e.target.elements.note.value]);
-    addNoteMutation({
-      variables: { id: params.id, note: e.target.elements.note.value },
-      refetchQueries: [{ query: QUERY_PLANT, variables: { id: params.id } }],
-    })
-      .then(() => loadPlants())
-      .finally(() => e.target.reset());
-  };
+	const handleAddNote = (e) => {
+		e.preventDefault();
+		setNotes([...(notes ?? []), e.target.elements.note.value]);
+		addNoteMutation({
+			variables: { id: params.id, note: e.target.elements.note.value },
+			// refetchQueries: [{ query: QUERY_PLANT, variables: { id: params.id } }],
+		}).finally(() => e.target.reset());
+	};
 
-  const loadPlants = useCallback(async () => {
-    query({ variables: { id: params.id } }).then((res) => {
-      console.log("plant updated:", res.data.plant);
-      setPlant(res.data.plant);
-    });
-  }, [query, params]);
+	const loadPlants = useCallback(async () => {
+		query({ variables: { id: params.id } });
+	}, [query, params]);
 
-  const deletePlantNote = (id, noteId) => {
-    console.log("delete plant note", noteId);
-    deletePlantNoteMutation({ variables: { id, noteId }, refetchQueries: [{ query: QUERY_PLANT, variables: { id: params.id } }] }).then(
-      () => loadPlants()
-    );
-  };
+	const deletePlantNote = (id, noteId) => {
+		console.log('delete plant note', noteId);
+		deletePlantNoteMutation({ variables: { id, noteId } });
+	};
 
-  useEffect(() => {
-    if (!params.id) {
-      return;
-    }
+	useEffect(() => {
+		if (!params.id) {
+			return;
+		}
 
-    loadPlants();
-  }, [params, loadPlants]);
+		loadPlants();
+	}, [params, loadPlants]);
 
-  if (loading) {
-    return <CenteredCircularProgress />;
-  }
+	if (loading) {
+		return <CenteredCircularProgress />;
+	}
 
-  if (error) {
-    alert("Error loading plant details: " + error.message);
-  }
+	if (error) {
+		alert('Error loading plant details: ' + error.message);
+	}
 
-  console.log({ plant });
-  if (!plant?.commonName) {
-    return <Typography variant="h1">No plant selected</Typography>;
-  }
+	if (!plant?.commonName) {
+		return <Typography variant='h1'>No plant selected</Typography>;
+	}
 
-  return (
-    <Stack direction="row" sx={{ placeContent: "center", padding: 5 }}>
-      <Paper
-        elevation={5}
-        component={Stack}
-        gap={3}
-        sx={{ width: "clamp(300px,60%,800px)", borderRadius: "5px", overflow: "hidden", display: "grid", gridTemplateColumns: "1fr 1fr" }}
-      >
-        <Box sx={{ height: "300px" }}>
-          <ResponsiveImageContainer image={plant.img} />
-        </Box>
+	return (
+		<Stack direction='row' sx={{ placeContent: 'center', padding: 5 }}>
+			<Paper
+				elevation={5}
+				component={Stack}
+				gap={3}
+				sx={{ width: 'clamp(300px,60%,800px)', borderRadius: '5px', overflow: 'hidden', display: 'grid', gridTemplateColumns: '1fr 1fr' }}
+			>
+				<Box sx={{ height: '300px' }}>
+					<ResponsiveImageContainer image={plant.img} />
+				</Box>
 
-        <Stack direction="column" p={2}>
-          <Typography variant="h1" component="h5">
-            {plant.commonName ?? "No name"}
-          </Typography>
+				<Stack direction='column' p={2}>
+					<Typography variant='h1' component='h5'>
+						{plant.commonName ?? 'No name'}
+					</Typography>
 
-          <span>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, rerum nam. Ipsam veniam asperiores cumque ullam? Dignissimos
-            asperiores dolorum nisi!
-          </span>
+					<span>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, rerum nam. Ipsam veniam asperiores cumque ullam? Dignissimos asperiores dolorum
+						nisi!
+					</span>
 
-          <Box sx={{ justifySelf: "end", marginTop: "auto", display: "flex" }}>
-            <Button variant="contained" sx={{ marginLeft: "auto" }}>
-              Update
-            </Button>
-          </Box>
-        </Stack>
+					<Box sx={{ justifySelf: 'end', marginTop: 'auto', display: 'flex' }}>
+						<Button variant='contained' sx={{ marginLeft: 'auto' }}>
+							Update
+						</Button>
+					</Box>
+				</Stack>
 
-        {/* text information */}
+				{/* text information */}
 
-        <Box component="form" onSubmit={handleAddNote} p={2} sx={{ display: "flex", flexDirection: "column", gap: 2, gridColumn: "1/3" }}>
-          <FormControl>
-            <TextareaAutosize placeholder="Add personal note" name="note"></TextareaAutosize>
-          </FormControl>
-          <Box>
-            <Button variant="contained" type="submit" sx={{ flex: "none", flexGrow: "none" }}>
-              Add Note
-            </Button>
-          </Box>
-        </Box>
+				<Box component='form' onSubmit={handleAddNote} p={2} sx={{ display: 'flex', flexDirection: 'column', gap: 2, gridColumn: '1/3' }}>
+					<FormControl>
+						<TextareaAutosize placeholder='Add personal note' name='note'></TextareaAutosize>
+					</FormControl>
+					<Box>
+						<Button variant='contained' type='submit' sx={{ flex: 'none', flexGrow: 'none' }}>
+							Add Note
+						</Button>
+					</Box>
+				</Box>
 
-        {/* list of personal notes, will be store if save button is clicked */}
-        <Stack direction="column" p={2} gap={2}>
-          {plant?.plantNotes?.map((noteObj) => (
-            <Stack key={noteObj.noteId} direction="row">
-              <Typography sx={{ display: "grid", placeItems: "center" }}>{noteObj.note}</Typography>
-              <IconButton onClick={(e) => deletePlantNote(plant._id, noteObj.noteId)}>
-                <Delete />
-              </IconButton>
-            </Stack>
-          ))}
-        </Stack>
-      </Paper>
-    </Stack>
-  );
+				{/* list of personal notes, will be store if save button is clicked */}
+				<Stack direction='column' p={2} gap={2}>
+					{plant?.plantNotes?.map((noteObj) => (
+						<Stack key={noteObj.noteId} direction='row'>
+							<Typography sx={{ display: 'grid', placeItems: 'center' }}>{noteObj.note}</Typography>
+							<IconButton onClick={(e) => deletePlantNote(plant._id, noteObj.noteId)}>
+								<Delete />
+							</IconButton>
+						</Stack>
+					))}
+				</Stack>
+			</Paper>
+		</Stack>
+	);
 }
 
 export default PlantDetails;
