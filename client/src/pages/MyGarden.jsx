0e2130a979a42db9d7b4755dc10afaa2db8c9c35
KateMcRo <katemcr0@gmail.com>
2023-07-25 10:22:03 -0700

    rearranged notes and made details mobile friendly

--- client/src/pages/MyGarden.jsx
+++ client/src/pages/MyGarden.jsx
@@ -20,17 +20,14 @@ export default function MyGarden() {
   }
 
   return (
-    <div style={{ margin: "20px"}}>
+    <div style={{ margin: "20px" }}>
       <div>
-        <span style={{ fontSize: "32px", fontFamily: "var(--bodyFont)" }}>
-          Hello,
-        </span>
+        <span style={{ fontSize: "32px" }}>Hello,</span>
         <br />
         <span
           style={{
             fontSize: "32px",
             fontWeight: "bold",
-            fontFamily: "var(--bodyFont)",
           }}
         >
           {username}!
@@ -38,22 +35,21 @@ export default function MyGarden() {
         <br />
         <br />
       </div>
-      <Grid container spacing={2} >
-  {data &&
-    data.user.plants?.map((plant) => (
-      <Grid item xs={12} sm={6} md={4} key={plant._id} >
-        <img
-          src={plant.img}
-          alt={plant.commonName}
-          onClick={() => {
-            handleClick(plant._id);
-          }}
-          style={{ borderRadius: '20px' }}
-        />
+      <Grid container spacing={2}>
+        {data &&
+          data.user.plants?.map((plant) => (
+            <Grid item xs={12} sm={6} md={4} key={plant._id}>
+              <img
+                src={plant.img}
+                alt={plant.commonName}
+                onClick={() => {
+                  handleClick(plant._id);
+                }}
+                style={{ borderRadius: "20px" }}
+              />
+            </Grid>
+          ))}
       </Grid>
-    ))}
-</Grid>
-
     </div>
   );
 }
--- client/src/pages/PlantDetails.jsx
+++ client/src/pages/PlantDetails.jsx
@@ -5,8 +5,9 @@ import { useEffect, useState } from "react";
 import { Button, IconButton, TextField } from "@mui/material";
 import { DELETE_PLANT } from "../utils/mutations";
 import useAuthService from "../utils/authHook";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import "./plantDetails.css";
 
 export default function PlantDetails() {
   const navigate = useNavigate("/mygarden");
@@ -41,52 +42,106 @@ export default function PlantDetails() {
     deletePlantById({ variables: { id: plantId } });
   }
   return (
-    <div
-      style={{
-        marginLeft: "20px",
-        display: "flex",
-        justifyContent: "space-around",
-      }}
-    >
-      <div style={{ padding: "20px" }}>
-        <h1>{singlePlantData?.plant?.latinName}</h1>
-        <a
-          href={singlePlantData?.plant?.img}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <img src={singlePlantData?.plant?.img} alt="Plant" style={{ borderRadius: "10px" }} />
-        </a>
+    <div style={{ width: "100%" }}>
+      <div
+        className="plant-details-column"
+        style={{
+          marginLeft: "20px",
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div style={{ padding: "20px" }}>
+          <h1>{singlePlantData?.plant?.latinName}</h1>
+          <a
+            href={singlePlantData?.plant?.img}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={singlePlantData?.plant?.img}
+              alt="Plant"
+              style={{ borderRadius: "10px" }}
+            />
+          </a>
+        </div>
+        <div>
+          <div style={{ padding: "40px" }}>
+            <h2>Common Name:</h2>
+            <p style={{ fontSize: "22px" }}>
+              {singlePlantData?.plant?.commonName}
+            </p>
+            <h2>Ideal Light:</h2>
+            <p style={{ fontSize: "22px" }}>
+              {singlePlantData?.plant?.idealLight}
+            </p>
+            <h2>Watering:</h2>
+            <p style={{ fontSize: "22px" }}>
+              {singlePlantData?.plant?.watering}
+            </p>
+            <Button variant="contained" color="success" onClick={deletePlant}>
+              DELETE PLANT
+            </Button>
+          </div>
+        </div>
       </div>
-      <div>
-        <div style={{ padding: "40px" }}>
-          <h2>Common Name:</h2>
-          <p>{singlePlantData?.plant?.commonName}</p>
-          <h2>Ideal Light:</h2>
-          <p>{singlePlantData?.plant?.idealLight}</p>
-          <h2>Watering:</h2>
-          <p>{singlePlantData?.plant?.watering}</p>
-         <p> <TextField 
-          id="outlined-multiline-flexible"
-          label="Add a note"
-          multiline
-          rows={4}
-          defaultValue=""
-        />
-        <IconButton aria-label="add" size="large" color="success">
-         <NoteAddIcon fontSize="inherit" />
-        </IconButton>
-          <IconButton aria-label="delete" size="large" color="error">
-         <DeleteForeverIcon fontSize="inherit" />
-        </IconButton>
-        </p>
-         
-          <Button variant="contained" color="success" onClick={deletePlant}>
-            DELETE PLANT
+      <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            margin: "20px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {" "}
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Add a note"
+            multiline
+            rows={4}
+            defaultValue=""
+            style={{
+              width: "100%",
+              backgroundColor: "#9ad7b5",
+              marginBottom: "10px",
+              alignSelf: "center",
+            }}
+          />
+          <Button
+            variant="contained"
+            color="success"
+            style={{ width: "200px", alignSelf: "center" }}
+            onClick={deletePlant}
+          >
+            ADD NOTE
           </Button>
+        </div>
+        <div
+          style={{
+            margin: "20px",
+            backgroundColor: "#cae6d5",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div>Plant notes will populate here with buttons to edit/delete</div>
+          <div>
+            <IconButton aria-label="add" size="large" color="success">
+              <NoteAddIcon fontSize="inherit" />
+            </IconButton>
+            <IconButton aria-label="delete" size="large" color="error">
+              <DeleteForeverIcon fontSize="inherit" />
+            </IconButton>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
